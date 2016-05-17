2c1c61d9f7963111a1f8303e3f79e64bf72702a3
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2016-05-17 12:25:05 +0200

    Add plots for cone transition

---
+++ gfx/cone/transition/plot_transition.py
@@ -0,0 +1,127 @@
+import style
+style.setup()
+
+from matplotlib.patches import Polygon
+import numpy as np
+import matplotlib.pyplot as plt
+import os, sys
+from matplotlib import ticker
+from glob import glob
+import tables as tb
+import pycurb.analysis as pa
+from scipy import stats
+import itertools as it
+import itertools
+from scipy import optimize
+
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
+cmap = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf']
+markers = itertools.cycle('o^*')
+
+from cycler import cycler
+
+cc = itertools.cycle(plt.cm.spectral(np.linspace(0,1,10)))
+#plt.rc('axes', prop_cycle=(cycler('color', cmap)))
+
+def get_amp(series):
+    n = len(series)/4
+    data = series[3*n:]
+    A = 0.5*(np.max(data)-np.min(data))
+    return A
+
+def main():
+    dpath = '/home/upgp/jruebsam/simulations/mai16/week2/move_border'
+    rs = np.linspace(0, 0.5, 5)[::-1]
+    cdir = os.getcwd()
+
+    f, axes = plt.subplots(len(rs), 2, figsize=style.figsize(0.9, np.sqrt(2)),
+                            gridspec_kw = {'width_ratios':[3, 1]})
+
+    l =  0.5*np.tan(np.pi/3.)
+    print l
+    for ax, radius in zip(axes[:, 1], rs):
+        r = radius
+        hc = 0.5*np.tan(np.pi/3.)
+        p00 = (0.5-r, 0)
+        p01 = (0.5+r, 0)
+        p11 = (1,hc)
+        p10 = (0, hc)
+        po2= (1, 1)
+        po = (0, 1)
+        cube_lines = [p00, p01, p11, po2, po, p10]
+        x = [p[0] for p in cube_lines]
+        y = [p[1] for p in cube_lines]
+        d = np.column_stack((x,y))
+        p = Polygon(d, alpha=0.5, color='b', hatch='x')
+        ax.add_artist(p)
+        ax.set_xticks([0, 1])
+        ax.set_yticks([0, 1])
+        ax.set_xlabel('x', labelpad = 0.1)
+        ax.set_ylabel('z', labelpad = 0.1)
+        ax.set_aspect('equal')
+
+
+    for i, (ax, radius) in enumerate(zip(axes[:, 0], rs[::-1])):
+
+        os.chdir(os.path.join(dpath, 'data', 'wall_%.3f' % radius))
+
+        simpathes =  glob('*/*.ekin')
+
+        omgs = sorted([float((x.split('/')[0]).split('_')[-1]) for x in simpathes])
+
+        a_ekin, a_vz, a_vphi = [], [], []
+        omgsn = []
+        for i, simpath in enumerate(sorted(simpathes)):
+            print simpath
+            data = np.genfromtxt(simpath)
+            time = data[:, 0]
+            ekin = data[:, 1]
+            vz   = data[:, -3]
+            vphi = data[:, -1]
+            #ax.plot(time, vz, label=simpath)
+            #a_ekin.append(get_amp(ekin))
+
+            amp = get_amp(vz)
+            #a_vphi.append(get_amp(vphi))
+
+            #plt.plot(time, vz, label=simpath.split('/')[-1])
+            a_vz.append(amp)
+            omgsn.append(omgs[i])
+
+        ax.plot(omgsn, a_vz, 'o--', ms=5, mew=0, alpha=0.8)
+
+        if i>0:
+            ax.axvline(2*np.cos(np.pi/2. -np.arctan(radius/l)), color='r', lw=0.75)
+        ax.set_ylabel(radius)
+        os.chdir(cdir)
+
+        formatter = ticker.ScalarFormatter(useMathText=True)
+        formatter.set_scientific(True)
+        formatter.set_powerlimits((-2,-3))
+        ax.yaxis.set_major_formatter(formatter)
+
+        ax.set_ylabel(r'$v_z^2$')
+        ax.grid(True)
+        ax.set_ylim(0, 1e-3)
+
+    for i, ax in enumerate(axes[:, 0]):
+        if i < 4:
+            labels = [item.get_text() for item in ax.get_xticklabels()]
+            empty_string_labels = ['']*len(labels)
+            ax.set_xticklabels(empty_string_labels)
+        else:
+            ax.set_xlabel(r'$\omega$')
+
+    """
+    plt.subplots_adjust(top=0.8, bottom =0.15)
+    ax.legend(ncol = 3, fontsize=8, loc='upper center', bbox_to_anchor=(0.5, 1.3),
+           fancybox=True, shadow=True)
+    """
+
+
+    plt.savefig('transition.pdf')
+
+if __name__=='__main__':
+    main()
